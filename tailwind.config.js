b482d0c05aa08495c8e8c76c40994ada511743aa
Akumar4318 <akumar4318@gmail.com>
2025-01-16 22:52:02 +0530

    Added Readme File

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      'SegoeUI':['Segoe UI'],
+      'SegoeBold':['Segoe UI Bold'],
+      'SegoeBolditalic':['Segoe UI Bold italic'],
+      'SegoeItalic':['Segoe UI Italic']
+    }
+   
+  },
+  plugins: [],
+}
+
